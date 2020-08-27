dcdc0c0705577bb8a810893d1587472607ea2411
Leopoldo Caballero <git@pitakill.net>
2020-08-27 18:04:42 -0500

    Add a basic offline mode (via PWA)
    1. Why is this change necessary?
    Show the PWA capabilities
    
    2. How does it address the issue?
    Register the PWA and shows the capabilites of the offline mode
    
    3. What side effects does this change have?
    None

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -4,23 +4,39 @@ import Home from './Home';
 import Filter from './Filter';
 import Main from './Main';
 import Menu from './Menu';
+import Offline from './Offline';
 
 function Router({ menu }) {
+  const [isOnline, setIsOnline] = React.useState(false);
+  
+  React.useEffect(() => {
+    fetch("https://google.com", { mode: 'no-cors' })
+      .then(() => setIsOnline(true))
+      .catch(() => setIsOnline(false))
+  });
+
   return (
     <BrowserRouter>
-      <Menu items={ menu } />
-      <Main>
-        <Route exact path="/" component={ Home } />
-        {
-          menu.map(m =>
-            <Route 
-              key={m}
-              path={`/${m.toLowerCase()}`} 
-              component={ Filter }
-            />
-          )
-        }
-    </Main>
+      {
+        isOnline
+        ? 
+          <>
+          <Menu items={ menu } />
+            <Main>
+              <Route exact path="/" component={ Home } />
+              {
+                menu.map(m =>
+                  <Route 
+                    key={m}
+                    path={`/${m.toLowerCase()}`} 
+                    component={ Filter }
+                  />
+                )
+              }
+            </Main>
+          </>
+        : <Offline />
+      }
   </BrowserRouter>
   )
 }
